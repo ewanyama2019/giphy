59fbb0328fc8e0272d3518530db16b47b1b2d376
ewanyama2019 <wafulawanyama@gmail.com>
2019-05-09 01:43:15 +0300

    installed rxjs 6.5.1 and imported the Observable from 'rxjs and map from 'rxjs/operators' in the  data.service.ts file

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
+export class ServiceNameService {
+
+  constructor(private http: HttpClient) { }
+
+}
+
+@Injectable({
+  providedIn: 'root'
+
+})
 export class DataService {
 
-  constructor() { }
+  // rxjs.of(1, 2, 3);
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('http://reqres.in/api/users')
+  }
 
   firstClick() {
     return console.log('clicked');
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +13,20 @@ export class HomeComponent implements OnInit {
 
   h1Style: boolean = false;
 
+  users: Object;
+
+  // rxjs.of(1, 2, 3);
+
+
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+        this.users = data
+        console.log(this.users);
   }
+    );
+}
 
   firstClick() {
     this.h1Style = true;
