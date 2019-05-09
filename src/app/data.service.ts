d82a8dd3f312a71f81a9840d7376b0e62fe880de
ewanyama2019 <wafulawanyama@gmail.com>
2019-05-09 12:45:12 +0300

    removed ul and li tags from the home.component.html and added divs with columns and rows

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -24,7 +24,7 @@ export class DataService {
    constructor(private http: HttpClient) { }
 
   getUsers() {
-    return this.http.get('https://reqres.in/api/users')
+    return this.http.get('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=UA48g9z6GbD3sZlqYNVG0Isv61PJSZVH&limit=5');
     }
 
   firstClick() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,22 +11,16 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
-  users: Object;
+   users: Object;
 
-    constructor(private data: DataService) { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getUsers().subscribe(data => {
         this.users = data
         console.log(this.users);
-  }
-    );
+   }
+  );
 }
 
-  firstClick() {
-    this.h1Style = true;
-    // console.log('clicked');
-    this.data.firstClick();
-  }
 }
